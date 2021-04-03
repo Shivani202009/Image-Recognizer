ca5403da62e453b8814588994046a75675991f6f
Shivani202009 <73746302+Shivani202009@users.noreply.github.com>
2021-04-03 18:29:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,3 +21,18 @@ var classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/mod
 function modelloaded() {
     console.log("hello Shivani your model has loaded")
 }
+
+function identify() {
+    img= document.getElementById("captureimage")
+    classifier.classify(img,getresults)
+}
+
+function getresults(error,results) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(results)
+        document.getElementById("objectresult").innerHTML=results[0].label
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3)*100+" %"
+    }
+}
